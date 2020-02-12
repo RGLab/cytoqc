313953fddda69f4c082c354136407636f87c5bea
mikejiang <wjiang2@fhcrc.org>
2020-02-11 17:08:03 -0800

    use hybrid approach (i.e. substr matching + approximate string matching) to improve the robustness of cqc_match

--- R/cqc_recommend.R
+++ R/cqc_recommend.R
@@ -36,49 +36,94 @@ cqc_recommend.cqc_match_result_gate <- function(x, ...) {
 #' }
 #' @param x A CQC object of some kind. See vignettes.
 #' @param max.distance Maximum distance allowed for a match. See ?agrep
+#' @param partial whether to do the partial sub string matching before the approximate string matching
 #' @param ... additional arguments not for the user.
 #' @importFrom tibble tibble add_row
 #' @importFrom utils adist
-cqc_recommend.cqc_match_result <- function(x, max.distance = 0.1, ...) {
+cqc_recommend.cqc_match_result <- function(x, max.distance = 0.1, partial = TRUE, ...) {
   res <- map_dfr(x, function(check_result) {
-    unknown <- check_result[["unknown"]]
-    missing <- check_result[["missing"]]
+    targets_queue <- targets <- check_result[["unknown"]]
+    refs_queue <- refs <- check_result[["missing"]]
     df <- tibble(from = character(), to = character())
-    # iteratively try to find the aproximate match between two vecs
-    while (length(unknown) > 0) {
-      if (length(missing) > 0) {
-        # Levenshtein (edit) distance
-        dist_mat <- adist(unknown, missing, ignore.case = TRUE)
-        nrows <- nrow(dist_mat)
-        ncols <- ncol(dist_mat)
-        # pick the best match
-        idx <- which.min(dist_mat)
-        # browser()
-        # get x, y coordinates
+
+    if (length(refs_queue) > 0 && length(targets_queue) > 0)
+    {
+
+
+      # Levenshtein (edit) distance
+      dist_mat <- adist(refs, targets, ignore.case = TRUE)
+
+      # check if each match passes the max.distance check
+      # agrep can be avoided if the formula of max.distance used by agrep is figured out
+      is_pass_mat <- do.call(rbind, sapply(refs, function(ref){
+                                     sapply(targets, function(target){
+                                        #default definition for max.distance of agrep is non-symetric
+                                        is.match <- agrepl(ref, target, ignore.case = TRUE, max.distance = max.distance)
+                                        is.match || agrepl(target, ref, ignore.case = TRUE, max.distance = max.distance)
+
+                                       })
+                                    }, simplify = FALSE)
+                            )
+
+      #check if substring
+      if(partial)
+      {
+        is_substring_mat <- do.call(rbind, sapply(refs, function(ref){
+          sapply(targets, function(target){
+            grepl(paste0("\\Q", ref, "\\E"), target, ignore.case = TRUE)||grepl(paste0("\\Q", target, "\\E"), ref, ignore.case = TRUE)
+
+          })
+        }, simplify = FALSE)
+        )
+      }else#create dummy mat
+        is_substring_mat <- matrix(TRUE, nrow = length(refs), ncol = length(targets), dimnames = list(refs, targets))
+
+      nrows <- nrow(dist_mat)
+      ncols <- ncol(dist_mat)
+
+
+      #first scan the substr matched entries
+      is_sub_idx <- which(is_substring_mat)
+      #order by dist
+      is_sub_idx <- is_sub_idx[order(dist_mat[is_sub_idx])]
+      #then scan the non-substr  entries
+      no_sub_idx <- which((!is_substring_mat)&is_pass_mat)#filter by dist threshold mat
+      no_sub_idx <- no_sub_idx[order(dist_mat[no_sub_idx])]
+      for(idx in c(is_sub_idx, no_sub_idx))
+      {
+        if (length(refs_queue) == 0 || length(targets_queue) == 0)
+          break#terminate the loop if one of the queues is empty
+
+          # get x, y coordinates
         ridx <- idx %% nrows
         if (ridx == 0) {
           ridx <- nrows
         }
         cidx <- ceiling(idx / nrows)
         # get the pair
-        from <- unknown[ridx]
-        to <- missing[cidx]
-        # pop the processed item
-        unknown <- unknown[-ridx]
-        missing <- missing[-cidx]
-        # check if exceeds max.distance
-        # agrep can be avoided if the formula of max.distance used by agrep is figured out
-        ind <- agrep(from, to, ignore.case = TRUE, max.distance = max.distance)
-        if (length(ind) > 0) { #
+        from <- targets[cidx]
+        to <- refs[ridx]
+        tind <- match(from, targets_queue)
+        rind <- match(to, refs_queue)
+
+        if(!is.na(tind) && !is.na(rind) )#skip already-matched pairs
+        {
           df <- add_row(df, from = from, to = to)
-        } else {
-          next
-        } # otherwise stop the matching process
-      } else # extra params
-      {
-        df <- add_row(df, from = unknown, to = NA)
-        unknown <- character()
-      }
+
+          # pop the processed item
+          refs_queue <- refs_queue[-rind]
+          targets_queue <- targets_queue[-tind]
+        }
+
+      }#end for
+    }
+
+    # iteratively try to find the aproximate match between two vecs
+    if (length(targets_queue) > 0 && length(refs_queue) == 0) {
+
+         df <- add_row(df, from = targets_queue, to = NA)
+         targets_queue <- character()
+
     }
     df
   }, .id = "group_id") %>% mutate(group_id = as.integer(group_id))
