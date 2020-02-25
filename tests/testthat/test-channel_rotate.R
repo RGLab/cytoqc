0c332a86f2d51e7dbcd6c3a8d4143a26040f1ea7
mikejiang <wjiang2@fhcrc.org>
2020-02-25 13:11:01 -0800

    update test case

--- tests/testthat/test-channel_rotate.R
+++ tests/testthat/test-channel_rotate.R
@@ -1,10 +1,10 @@
 context("cqc_check")
 library(flowCore)
 data(GvHD)
-cflist <- fsApply(GvHD[1:2], function(fr)flowFrame_to_cytoframe(fr[1:2, c(1:3,6)]), simplify = FALSE)
-cqc_data <- cqc_cf_list(cflist)
 
 test_that("cqc_fix_panel -- rotated", {
+  cflist <- fsApply(GvHD[1:2], function(fr)flowFrame_to_cytoframe(fr[1:2, c(1:3,6)]), simplify = FALSE)
+  cqc_data <- cqc_cf_list(cflist)
   #fix rotated chnl
   cf <- cqc_data[[1]]
   su <- range(cf, "data")
@@ -14,7 +14,9 @@ test_that("cqc_fix_panel -- rotated", {
   newcn[3] <- "D"
   colnames(cf) <- newcn
   cf <- cf[,4:1]
-
+  #craft a spillover matrix
+  sp <- matrix(rnorm(9), nrow = 3, dimnames = list(NULL, newcn[1:3]))
+  keyword(cf)[["SPILL"]] <- sp
   groups <- cqc_check(cqc_data, "panel")
   expect_equal(ncol(format(groups, "marker")), 3)
 
@@ -23,16 +25,20 @@ test_that("cqc_fix_panel -- rotated", {
   expect_equal(colnames(cqc_data[[2]]),  cn)
   #make sure the underlying data is correct as well
   expect_equal(range(cf, "data")[,colnames(su)], su)
-  #fix rotated marker
-  # pnl <- cf_get_panel(cqc_data[[1]])
-  # cn <- pnl[["marker"]]
-  # newcn <- cn[c(2,3,1)]
-  # names(newcn) <- pnl[["channel"]]
-  # markernames(cqc_data[[1]]) <- newcn
-  # groups <- cqc_check(cqc_data, "panel")
-  # expect_equal(ncol(format(groups, "marker")), 3)
-  #
-  # cqc_fix_panel(groups, 1, "marker")
-  # expect_equal(colnames(cqc_data[[1]]),  newcn)
+  expect_equal(colnames(keyword(cf)[["SPILL"]]), cn[1:3])
 
-})
+  #fix rotated marker
+  pnl <- cf_get_panel(cqc_data[[1]])
+  marker <- pnl[["marker"]]
+  newmarker <- marker[c(2,3,1)]
+  names(newmarker) <- pnl[["channel"]][1:3]
+  markernames(cqc_data[[1]]) <- newmarker
+  groups <- cqc_check(cqc_data, "panel")
+  expect_equal(ncol(format(groups)), 3)
+
+  cqc_fix_panel(groups, 1, "marker")
+  pnl <- cf_get_panel(cqc_data[[1]])
+  expect_equal(pnl,  cf_get_panel(cqc_data[[2]]))
+
+
+  })
